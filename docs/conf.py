b415e13c7ffcf5d804b01a117ed853c9be0e69f1
giadarol <giovanni.iadarola@cern.ch>
2022-06-09 20:42:31 +0200

    Quick push

--- docs/conf.py
+++ docs/conf.py
@@ -151,7 +151,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Xsuite'
-copyright = u'2014, Read the Docs'
+copyright = u'2021, CERN'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
