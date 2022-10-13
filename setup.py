b33e622379aaa576f6e4c8e5f37400b2a15528a2
Szymon Łopaciuk <szymon@lopaciuk.eu>
2022-10-13 10:38:22 +0200

    add single-sourced version
    Signed-off-by: Szymon Łopaciuk <szymon@lopaciuk.eu>

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages, Extension
+from pathlib import Path
 
 #######################################
 # Prepare list of compiled extensions #
@@ -11,14 +12,20 @@ extensions = []
 # Setup #
 #########
 
+version_file = Path(__file__).parent / 'xsuite/_version.py'
+dd = {}
+with open(version_file.absolute(), 'r') as fp:
+    exec(fp.read(), dd)
+__version__ = dd['__version__']
+
 setup(
     name='xsuite',
-    version='0.4.0',
+    version=__version__,
     description='Integrated suite for particle accelerator simulations',
     long_description='Integrated suite for particle accelerator simulations',
     author='R. De Maria, G. Iadarola et al.',
     packages=find_packages(),
-    ext_modules = extensions,
+    ext_modules=extensions,
     install_requires=[
         'numpy>=1.0',
         'xobjects',
