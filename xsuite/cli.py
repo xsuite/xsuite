ee6caefc308ede752796ef69111860d62ebaeb8a
giadarol <giovanni.iadarola@cern.ch>
2022-12-14 15:38:19 +0100

    Added clean in cli

--- xsuite/cli.py
+++ xsuite/cli.py
@@ -2,9 +2,21 @@
 # This file is part of the Xobjects Package.  #
 # Copyright (c) CERN, 2022.                   #
 # ########################################### #
-from xtrack.prebuild_kernels import regenerate_kernels
+from xtrack.prebuild_kernels import clear_kernels, regenerate_kernels
+import argparse
 
 
 def main():
-    regenerate_kernels()
-    print('Successfully compiled kernels.')
+    parser = argparse.ArgumentParser(
+                    prog = 'ProgramName',
+                    description = 'What the program does',
+                    epilog = 'Text at the bottom of help')
+    parser.add_argument('--clean', action='store_true')
+    args = parser.parse_args()
+
+    if args.clean:
+        clear_kernels()
+        print('Cleaned kernels.')
+    else:
+        regenerate_kernels()
+        print('Successfully compiled kernels.')
