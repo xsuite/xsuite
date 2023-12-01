76729d845a913b1cdb2ebcdaf4b2f0aed5ad6b0d
Szymon Łopaciuk <szymon@lopaciuk.eu>
2023-12-01 17:01:37 +0100

    add run name, run_on_gh: allow giving workflow branch name

--- run_release_tests.sh
+++ run_release_tests.sh
@@ -1,9 +1,11 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
+WF_BRANCH="main"
+
 XOBJECTS=xsuite:main
    XPART=xsuite:main
    XDEPS=xsuite:main
-  XTRACK=xsuite:release/0.46.4
+  XTRACK=xsuite:main
  XFIELDS=xsuite:main
    XMASK=xsuite:main
    XCOLL=xsuite:main
@@ -11,10 +13,14 @@ XOBJECTS=xsuite:main
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cpu \
   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
 python run_on_gh.py --suites xm --platform alma-cpu --ctx cpu \
-  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform  alma-cpu --ctx cpu:auto \
-   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
+   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
+python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cpu \
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform alma --ctx cuda \
-  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cl \
-   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
+   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
+# python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform radeon --ctx cl \
+#   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
