a032d8938b459b0eebd024153662b6caefa5ce92
giadarol <giovanni.iadarola@cern.ch>
2023-12-06 12:27:12 +0100

    Status

--- run_release_tests.sh
+++ run_release_tests.sh
@@ -1,15 +1,13 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
 XOBJECTS=xsuite:main
-   XPART=xsuite:main
+   XPART=xsuite:api/rename_co_guess
    XDEPS=xsuite:main
-  XTRACK=xsuite:release/0.46.4
- XFIELDS=xsuite:main
+  XTRACK=xsuite:example/multiturn_twiss
+ XFIELDS=xsuite:feature/pizza_pic
    XMASK=xsuite:main
    XCOLL=xsuite:main
 
-python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cpu \
-  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
 python run_on_gh.py --suites xm --platform alma-cpu --ctx cpu \
   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform  alma-cpu --ctx cpu:auto \
@@ -18,3 +16,5 @@ python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform alma --ctx cuda \
   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cl \
    --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
+python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cpu \
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
