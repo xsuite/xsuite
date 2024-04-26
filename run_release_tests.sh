f881c1ed880d01b00104a376a909a9caf2a451c8
giadarol <giovanni.iadarola@cern.ch>
2024-04-26 15:24:51 +0200

    Update release test script

--- run_release_tests.sh
+++ run_release_tests.sh
@@ -2,24 +2,24 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 WF_BRANCH="main"
 
-XOBJECTS=xsuite:merge/compact_json
-   XPART=xsuite:refactor/adapt_to_xtrack_replicas
-   XDEPS=xsuite:feature/check_limits
-  XTRACK=xsuite:refactor/replicas
- XFIELDS=xsuite:refactor/nocompounds
+XOBJECTS=xsuite:main
+   XPART=xsuite:main
+   XDEPS=xsuite:main
+  XTRACK=xsuite:main
+ XFIELDS=xsuite:main
    XMASK=xsuite:main
    XCOLL=xsuite:main
 
 # GPU tests
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform alma --ctx cuda \
-  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --branch $WF_BRANCH
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cl \
-   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK
+   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --branch $WF_BRANCH
 
 # CPU tests
 python run_on_gh.py --suites xm --platform pcbe-abp-gpu001 --ctx cpu \
-  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --branch $WF_BRANCH
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform  radeon --ctx cpu:auto \
-   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --branch $WF_BRANCH
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform alma-cpu --ctx cpu \
-  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --branch $WF_BRANCH
+  --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --branch $WF_BRANCH
