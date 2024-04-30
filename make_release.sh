c72586bce6113e9a265cc76fdc5f4dda1db47e04
Szymon Łopaciuk <szymon@lopaciuk.eu>
2024-04-30 12:00:50 +0200

    Make a release script

--- make_release.sh
+++ make_release.sh
@@ -18,7 +18,39 @@ update_version () {
   sed -i '' -E -e "s/$package==[0-9.]+/$package==$version/g" "pyproject.toml"
 }
 
+verify_input() {
+  read -p "Are you sure you want to continue? [y/N] " prompt
+  if [[ $prompt != "y" && $prompt != "Y" ]]
+  then
+    exit 0
+  fi
+}
+
+echo "=== THE CURRENT TAGGED VERSION ==="
+git tag --sort=authordate | tail -n 1
+
+echo "=== BUMPING VERSIONS: VERIFY I DID IT CORRECTLY ==="
 for package in xtrack xfields xcoll xobjects; do
   latest_version=$(get_latest_version $package)
   update_version $package $latest_version
 done
+
+git diff -- pyproject.toml
+verify_input
+
+echo "=== WHAT SHOULD BE THE NEXT VERSION? ==="
+read -p "Next version: " next_version
+read -p "Retype the version: " retype_version
+
+if [[ $next_version != $retype_version ]]
+then
+  echo "Versions do not match. Exiting."
+  exit 1
+fi
+
+echo "=== WILL PROCEED TO TAG AND COMMIT NOW ==="
+git add pyproject.toml
+git commit -m "Bump versions"
+git tag -a $next_version -m "Release $next_version"
+
+echo "=== RUN 'git push' TO TRIGGER THE RELEASE ==="
