1c906a98bfb8964922b7889da223b8e27a4c7c14
Szymon Łopaciuk <szymon@lopaciuk.eu>
2024-10-23 17:20:15 +0200

    Move redirection $GITHUB_STEP_SUMMARY from the script to the wf scope

--- .github/scripts/generate_summary.sh
+++ .github/scripts/generate_summary.sh
@@ -5,30 +5,30 @@
 # ######################################### #
 set -xe
 
-echo "## Test Summary $1" >> $GITHUB_STEP_SUMMARY
+echo "## Test Summary $1"
 
 results_file="reports/$1/results.txt"
 
 if [ -f "$results_file" ]; then
-  echo "Results file found, processing..."
+  # Results file found, processing...
   sed -i -e 's/\x1b\[[0-9;]*m//g' -e 's/^> /\\> /g' $results_file
 
   passed=$(grep -c PASSED $results_file || true)
   failed=$(grep "FAILED.*xsuite" $results_file | grep -P 'FAILED.* - ' | sed -E 's/.*\] (.*::[^ ]+).* - .*/\1/' | sort | uniq | wc -l)
   skipped=$(grep -c SKIPPED $results_file || true)
 
-  echo "| Result     | Number   |" >> $GITHUB_STEP_SUMMARY
-  echo "|------------|----------|" >> $GITHUB_STEP_SUMMARY
-  echo "| Passed ✅  | $passed  |" >> $GITHUB_STEP_SUMMARY
-  echo "| Failed ❌  | $failed  |" >> $GITHUB_STEP_SUMMARY
-  echo "| Skipped ⏭️ | $skipped |" >> $GITHUB_STEP_SUMMARY
+  echo "| Result     | Number   |"
+  echo "|------------|----------|"
+  echo "| Passed ✅  | $passed  |"
+  echo "| Failed ❌  | $failed  |"
+  echo "| Skipped ⏭️ | $skipped |"
 
   if [ $failed -ne 0 ]; then
-    echo "### 🔴 Failed Tests" >> $GITHUB_STEP_SUMMARY
+    echo "### 🔴 Failed Tests"
     grep "FAILED.*xsuite" $results_file | grep -P 'FAILED.* - ' | sed -E 's/.*\] (.*::[^ ]+).* - (.*)/\1 - \2/' | sort | uniq | while read -r failure; do
-      echo "- $failure" >> $GITHUB_STEP_SUMMARY
+      echo "- $failure"
 
-      echo "<details><summary>Click here to view the error details</summary><code>" >> $GITHUB_STEP_SUMMARY
+      echo "<details><summary>Click here to view the error details</summary><code>"
       # Extract the failure name
       test_name=$(echo "$failure" | sed -E 's/.*::([^\-]+).* - .*/\1/' | sed -E 's/\x1b\[[0-9;]*m//g' )
       test_name=$(echo "$test_name" | sed 's/[][]/\\&/g')
@@ -41,12 +41,12 @@ if [ -f "$results_file" ]; then
         if [[ ! -z "$error_end_line" ]]; then
             # Find the end of the error block
             absolute_error_end_line=$((error_start_line + error_end_line - 1))
-            sed -n "${error_start_line},${absolute_error_end_line}p" $results_file | sed 's/^>/\\>/' >> $GITHUB_STEP_SUMMARY
+            sed -n "${error_start_line},${absolute_error_end_line}p" $results_file | sed 's/^>/\\>/'
         fi
       fi
-      echo "</code></details>" >> $GITHUB_STEP_SUMMARY
+      echo "</code></details>"
     done
   fi
 else
-  echo "No test results found." >> $GITHUB_STEP_SUMMARY
+  echo "No test results found."
 fi
