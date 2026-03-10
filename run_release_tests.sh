93887389835aeedbc23131469384659ab46a0cf2
Szymon Łopaciuk <szymon@lopaciuk.eu>
2026-03-10 11:16:42 +0100

    Replace the ubuntu runner with a new alma runner

--- run_release_tests.sh
+++ run_release_tests.sh
@@ -1,11 +1,11 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
-WF_BRANCH="feature/thick_cavity" # xsuite branch
+WF_BRANCH="main" # xsuite branch
 
 XOBJECTS=xsuite:main
    XPART=xsuite:main
    XDEPS=xsuite:main
-  XTRACK=xsuite:merge/misalign_optimize
+  XTRACK=xsuite:main
  XFIELDS=xsuite:main
    XMASK=xsuite:main
    XCOLL=xsuite:main
@@ -16,14 +16,16 @@ python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform alma --ctx cuda \
   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --xw $XWAKES --branch $WF_BRANCH \
   --pytest-opts "-m context_dependent"
 
-python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform ubuntu --ctx cl \
+python run_on_gh.py --suites xo,xp,xd,xt,xf,xc --platform alma --ctx cl \
    --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --xw $XWAKES --branch $WF_BRANCH \
    --pytest-opts "-m context_dependent"
 
-# # CPU tests
-# python run_on_gh.py --suites xm --platform alma-cpu-smaller --ctx cpu \
-#   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --xw $XWAKES --branch $WF_BRANCH
-# python run_on_gh.py --suites xo,xp,xd,xt,xf,xc,xw --platform  alma-cpu-2 --ctx cpu:auto \
-#   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --xw $XWAKES --branch $WF_BRANCH
+# CPU tests
+python run_on_gh.py --suites xm --platform alma-cpu-smaller --ctx cpu \
+   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --xw $XWAKES --branch $WF_BRANCH
+
+python run_on_gh.py --suites xo,xp,xd,xt,xf,xc,xw --platform  alma-cpu-2 --ctx cpu:auto \
+   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --xw $XWAKES --branch $WF_BRANCH
+
 python run_on_gh.py --suites xo,xp,xd,xt,xf,xc,xw --platform alma-cpu-1 --ctx cpu \
   --xo $XOBJECTS --xp $XPART --xd $XDEPS --xt $XTRACK --xf $XFIELDS --xm $XMASK --xc $XCOLL --xw $XWAKES --branch $WF_BRANCH
