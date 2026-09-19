b712eccd2c42db20e20e5948d4fe16f21e63e8fe
giadarol <giovanni.iadarola@cern.ch>
2026-09-19 06:19:59 +0200

    Switch documentation to book theme and refine highlighting and contents styling

--- docs/apireference.rst
+++ docs/apireference.rst
@@ -3,7 +3,8 @@ Reference guide
 ===============
 
 .. contents:: Table of Contents
-    :depth: 3
+    :depth: 2
+    :local:
 
 
 Settings (``xt.settings``)
--- docs/conf.py
+++ docs/conf.py
@@ -474,17 +474,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-html_logo = "figures/xsuite_logo_alpha.png"
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
+html_theme = 'sphinx_book_theme'
+html_logo = "figures/xsuite_logo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'pygments_light_style': 'tango',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -509,6 +507,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
